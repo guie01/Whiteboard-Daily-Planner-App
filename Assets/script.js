3a48ee984567d60b4129aba571ef608890f70f61
Guillermo Garcia <ggq0107@hotmail.com>
2021-01-21 15:47:45 -0500

    added css and styling to index.html, added script, and js to create time boxes

---
+++ Assets/script.js
@@ -0,0 +1,9 @@
+let timesArr= ["9AM","10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]; 
+
+for (let i=1; i<timesArr.length; i++){
+    let newTimeBlockEL= $("#time").clone();
+    newTimeBlockEL.attr("id", timesArr[i]); 
+    newTimeBlockEL.children(".row").attr("style", "white-space: pre-Wrap"); 
+    newTimeBlockEL.children(".row").children(".hour").text(timesArr[i]); 
+    newTimeBlockEL.appendTo(".container"); 
+}
