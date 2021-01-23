4ab51eae09f1ba8a114e886191b4f8c242932d95
Guillermo Garcia <ggq0107@hotmail.com>
2021-01-23 17:27:30 -0500

    added save button functionality, hoever only works for the first button. Fixing

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,6 @@
+// --- Generate new times blocks ---- //
+
+
 let timesArr= ["9AM","10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]; 
 
 for (let i=1; i<timesArr.length; i++){
@@ -7,3 +10,18 @@ for (let i=1; i<timesArr.length; i++){
     newTimeBlockEL.children(".row").children(".hour").text(timesArr[i]); 
     newTimeBlockEL.appendTo(".container"); 
 }
+
+
+
+// ---- Functionality of the save button ---- //
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    alert("Task Saved!");
+
+    var dailyTask = $("#inlineFormInput").val();
+    localStorage.setItem('userInput', dailyTask);
+    
+});
+
+$("#inlineFormInput").text(localStorage.getItem('userInput'));
